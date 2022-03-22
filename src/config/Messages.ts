35de02c720dad38114f5b3ca533f5e7d78f07964
YoussefRashad <youssefrashad119@gmail.com>
2022-03-22 20:05:12 +0200

    add messages for the auth validation

---
+++ src/config/Messages.ts
@@ -0,0 +1,14 @@
+export default {
+  user: {
+    error: {
+      INCORRECT_CREDENTIALS: 'Email/Phone or Password Is Incorrect',
+      PHONE_REQUIRED: 'Phone is required',
+      EMAIL_REQUIRED: 'Email is required',
+      EMAIL_VALID: 'Email is not valid',
+      USERNAME_REQUIRED: 'Username is required',
+      PASSWORD_REQUIRED: 'Password is required',
+      UNAUTHORIZED_TOKEN: "No token, authorization denied",
+      INVALIDED_TOKEN: "No token, authorization denied",
+    },
+  },
+}
