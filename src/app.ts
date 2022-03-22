345443a4c8f0cb66ef1247e4cf42ee024c89c78a
YoussefRashad <youssefrashad119@gmail.com>
2022-03-23 01:48:41 +0200

    use rate limit in news apis

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,21 @@ import Request from "./types/Request";
 import HttpStatusCodes from "http-status-codes";
 import userRouter from "./routes/api/user";
 import newsRouter from "./routes/api/news";
+import rateLimit from './middleware/rateLimit'
+import cors from 'cors'
 
 const app = express();
-// Main middleware
+
+// 1) GLOBAL MIDDLEWARES
+// Access-Control-Allow-Origin *
+app.use(cors());
+
+// Body parser, reading data from body into req.body
 app.use(express.json())
 
 // Routes
 app.use('/api/user', userRouter)
-app.use('/api/news', newsRouter)
+app.use('/api/news', rateLimit, newsRouter)
 
 // No route matched, 404 not found
 app.use((req: Request, res: Response, next: NextFunction)=>{
